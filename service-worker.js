a8e688be4b9e3bdbb8b85c0366b1a1f2bd8a2c19
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-18 09:55:17 -0600

    added in files to be stored in the cache

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,14 @@
 //!need some help with this section: 
 
-const { response } = require("express");
 
-//*I think I need to add all the file in a const FILES_TO_CACHE =[array]
+const FILES_TO_CACHE =[
+    "/",
+    "/index.html",
+    "/assets/css/styles.css",
+    "/assets/js/index.js",
+    "/assets/icons/icon-192x192.png",
+    "/assets/icons/icon-512x512.png"
+    ]
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 //added in the above to give cache and data cache variables
