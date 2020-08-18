74076d501931e4ab0913534534aec9b0a662503b
Shelby Handfield <shelbyhandfield@protonmail.com>
2020-08-18 15:19:37 -0600

    made edits to code

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,8 +3,7 @@ const FILES_TO_CACHE = [
     "/index.html",
     "/assets/css/styles.css",
     "/assets/js/index.js",
-    "/assets/icons/icon-192x192.png",
-    "/assets/icons/icon-512x512.png"
+    "/assets/js/db.js"
     ];
 
 const CACHE_NAME = "static-cache-v2";
@@ -14,31 +13,31 @@ const DATA_CACHE_NAME = "data-cache-v1";
 //*added in a install and access to Cache API
 self.addEventListener("install", (evt)=>{
     evt.waitUntil(
-        caches.open(CACHE_NAME).then((cache)=>{
-            cache.addAll(FILES_TO_CACHE)
+        caches.open(CACHE_NAME).then(cache=>{
             console.log("Your files were pre-cached successfully!")
+            return cache.addAll(FILES_TO_CACHE)
         }) //! need to double check this
-            .then(() => self.skipWaiting())
     );
+    self.skipWaiting();
 });
 
 //*this creates the code for the activation of the cache storage. By using keys, it will return all the subaches in the cache storage. It returns the data via an array of strings
 self.addEventListener("activate", (evt)=>{
     evt.waitUntil(
-        caches.keys().then((keylist) =>{
+        caches.keys().then(keylist =>{
             return Promise.all(
-                keylist.map((key) =>{
+                keylist.map(key =>{
                     if (key !== CACHE_NAME && key !== DATA_CACHE_NAME){
                         console.log("Removing old cache data", key);
                         return caches.delete(key);
                         //*only returns when the function is done with the cleanup of the old cache data
                     }
                 })
-            )
+            );
         })
-       .then(() => self.clients.claim())
-        //checks that service worker is functioning properly
     );
+       self.clients.claim()
+        //checks that service worker is functioning properly
 });
 
 //this is for fetching the data if the request is not in the cache then it will use other origins such as url
@@ -51,7 +50,7 @@ self.addEventListener("fetch", (evt) => {
     }
     //this will GET the request from data from the api routes
     //! should it be /api/ or /api/transactions?
-    if (evt.request.url.includes ("/api/transactions")){
+    if (evt.request.url.includes ("/api/")){
         //created not only a network request but also a fallback when offline
         evt.respondWith(
             caches.open(DATA_CACHE_NAME).then(cache => {
